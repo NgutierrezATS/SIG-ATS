cca55a529e41493197cb65fc59883ba6de08e455
rrojas <nrojasm.ua@gmail.com>
2018-07-10 20:04:10 -0400

    commit

--- application/views/templates/header.php
+++ application/views/templates/header.php
@@ -13,6 +13,7 @@
     <link rel="stylesheet" href="<?php echo base_url()."assets/"; ?>css/fuente.css">
     <link rel="stylesheet" href="<?php echo base_url()."assets/"; ?>slider/css/estilos.css">
     <link rel="stylesheet" href="<?php echo base_url()."assets/"; ?>chatbox/style.css">
+    <link rel="stylesheet" href="<?php echo base_url()."assets/"; ?>simple-sidebar.css">
     <link rel="stylesheet" href="<?php echo base_url()."assets/"; ?>select2/select2.min.css">
         <link rel="stylesheet" href="<?php echo base_url()."assets/"; ?>bower_components/slider/css/slider.css">
 
